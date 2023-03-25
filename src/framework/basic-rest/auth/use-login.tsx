b48d09e267159f705d533f726340dceb616004aa
Avimonnu Arnob <avimonnuarnob@gmail.com>
2023-03-25 13:13:24 +0600

    init app

---
+++ src/framework/basic-rest/auth/use-login.tsx
@@ -0,0 +1,35 @@
+import { useModalAction } from '@components/common/modal/modal.context';
+import { useUI } from '@contexts/ui.context';
+import Cookies from 'js-cookie';
+import { useMutation } from 'react-query';
+
+export interface LoginInputType {
+  email: string;
+  password: string;
+  remember_me: boolean;
+}
+// eslint-disable-next-line @typescript-eslint/require-await
+async function login(input: LoginInputType) {
+  return {
+    token: `${input.email}.${input?.remember_me?.toString()}`
+      .split('')
+      .reverse()
+      .join(''),
+  };
+}
+export const useLoginMutation = () => {
+  const { authorize } = useUI();
+  const { closeModal } = useModalAction();
+
+  return useMutation((input: LoginInputType) => login(input), {
+    onSuccess: (data) => {
+      Cookies.set('auth_token', data.token);
+      authorize && authorize();
+      closeModal();
+    },
+    onError: (data) => {
+      // eslint-disable-next-line no-console
+      console.log(data, 'login error response');
+    },
+  });
+};
